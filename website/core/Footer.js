7d2eb852aeffabd0f00d66e1c2abd731a3ddcffe
rouzbeh <rouzbeh.sarrafieh@gmail.com>
2019-10-02 03:37:22 -0700

    fix: link to getting started and readme contrib steps

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -37,7 +37,7 @@ class Footer extends React.Component {
           </a>
           <div>
             <h5>Docs</h5>
-            <a href={this.docUrl("start.html", this.props.language)}>
+            <a href={this.docUrl("start.html")}>
               Getting Started
             </a>
           </div>
